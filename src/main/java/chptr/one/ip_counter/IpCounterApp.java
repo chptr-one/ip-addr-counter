22c16513d3aeedccb8f5de2fde005482c488e4c8
chptr_one <peter.vakulin@protonmail.com>
2021-04-25 22:32:35 +0300

    Parsing args to Optional. Reformat output.

--- src/main/java/chptr/one/ip_counter/IpCounterApp.java
+++ src/main/java/chptr/one/ip_counter/IpCounterApp.java
@@ -2,6 +2,7 @@ package chptr.one.ip_counter;
 
 import me.tongfei.progressbar.ProgressBar;
 import me.tongfei.progressbar.ProgressBarBuilder;
+import me.tongfei.progressbar.ProgressBarStyle;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -9,29 +10,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class IpCounterApp {
 
-    public static void main(String[] args) {
-        Path path = IpCounterAppUtils.checkArgsAngParsePath(args);
-        if (path == null) {
-            return;
-        }
-
+    private static void doTheWork(String fileName) {
         ProgressBarBuilder pbb = new ProgressBarBuilder()
-                .setTaskName("Reading " + path.getFileName())
+                .setTaskName("Processing " + Path.of(fileName).getFileName())
                 .setUnit("MB", 1048576); // setting the progress bar to use MB as the unit
 
         long startTime = System.nanoTime();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
-                ProgressBar.wrap(new FileInputStream(String.valueOf(path)), pbb)))) {
-
+                ProgressBar.wrap(new FileInputStream(fileName), pbb)))) {
             IpStringCounter counter = new IpStringCounter(reader);
             counter.processFile();
             long totalTime = (System.nanoTime() - startTime);
-            IpCounterAppUtils.printResults(counter, Files.size(path), totalTime);
+            IpCounterAppUtils.printResults(counter, Files.size(Path.of(fileName)), totalTime);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Optional<String> optionalFileName = IpCounterAppUtils.checkArgsAndGetFileName(args);
+        optionalFileName.ifPresent(IpCounterApp::doTheWork);
     }
 }
--- src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
+++ src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
@@ -1,30 +1,30 @@
 package chptr.one.ip_counter;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 public class IpCounterAppUtils {
 
-    public static Path checkArgsAngParsePath(String[] args) {
+    public static Optional<String> checkArgsAndGetFileName(String[] args) {
         if (args.length != 2 || !"-file".equals(args[0])) {
             System.err.println("Wrong arguments. Use param -file file_name.");
-            return null;
+            return Optional.empty();
         }
-        Path filePath = Paths.get(args[1]);
-        if (!Files.exists(filePath)) {
-            System.err.println("File " + filePath + " does not exists.");
-            return null;
+        String fileName = args[1];
+        if (!Files.exists(Paths.get(fileName))) {
+            System.err.println("File " + fileName + " does not exists.");
+            return Optional.empty();
         }
-        return filePath;
+        return Optional.of(fileName);
     }
 
     public static void printResults(IpStringCounter counter, long fileSize, long totalTime) {
         double timeInSeconds = totalTime / 1_000_000_000.0;
         System.out.printf("Unique IP addresses : %,d\n", counter.getUniqueIp());
-        System.out.printf("Lines processed     : %,d lines\n", counter.getLinesProcessed());
+        System.out.printf("Lines processed     : %,d\n", counter.getLinesProcessed());
         System.out.printf("Total time          : %.3f sec\n", timeInSeconds);
-        System.out.printf("Processing speed    : %.2f KLines/sec\n", counter.getLinesProcessed() / 1000 / timeInSeconds);
-        System.out.printf("Average disk speed  : %.2f MB/sec\n", fileSize / 1024 / 1024 / timeInSeconds);
+        System.out.printf("Processing speed    : %.3f KLines/sec\n", counter.getLinesProcessed() / 1000 / timeInSeconds);
+        System.out.printf("Average disk speed  : %.3f MB/sec\n", fileSize / 1024 / 1024 / timeInSeconds);
     }
 }
