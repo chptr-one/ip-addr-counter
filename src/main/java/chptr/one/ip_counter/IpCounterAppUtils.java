d4db743350632466fa537752da8eae07d03b7030
chptr_one <peter.vakulin@protonmail.com>
2021-04-25 18:06:02 +0300

    Refactor -file checks

--- src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
+++ src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
@@ -7,16 +7,17 @@ import java.nio.file.Paths;
 public class IpCounterAppUtils {
 
     public static Path checkArgsAngParsePath(String[] args) {
-        if (args.length == 2 && "-file".equals(args[0])) {
-            Path filePath = Paths.get(args[1]);
-            if (Files.exists(filePath)) {
-                return filePath;
-            } else {
-                System.err.println("File " + filePath + "does not exists.");
-            }
-        } else {
+        if (!(args.length == 2) || !"-file".equals(args[0])) {
             System.err.println("Wrong arguments. Use param -file file_name.");
+            return null;
         }
-        return null;
+
+        Path filePath = Paths.get(args[1]);
+        if (!Files.exists(filePath)) {
+            System.err.println("File " + filePath + "does not exists.");
+            return null;
+        }
+
+        return filePath;
     }
 }
