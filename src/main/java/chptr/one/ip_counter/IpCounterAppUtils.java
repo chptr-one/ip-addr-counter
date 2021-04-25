0ed556739ab1aaa7d559e6ae7cf386568557eb6f
chptr_one <peter.vakulin@protonmail.com>
2021-04-25 21:54:59 +0300

    minor improvements

--- src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
+++ src/main/java/chptr/one/ip_counter/IpCounterAppUtils.java
@@ -7,17 +7,15 @@ import java.nio.file.Paths;
 public class IpCounterAppUtils {
 
     public static Path checkArgsAngParsePath(String[] args) {
-        if (!(args.length == 2) || !"-file".equals(args[0])) {
+        if (args.length != 2 || !"-file".equals(args[0])) {
             System.err.println("Wrong arguments. Use param -file file_name.");
             return null;
         }
-
         Path filePath = Paths.get(args[1]);
         if (!Files.exists(filePath)) {
-            System.err.println("File " + filePath + "does not exists.");
+            System.err.println("File " + filePath + " does not exists.");
             return null;
         }
-
         return filePath;
     }
 
