a59a6cebe70652cd922d9c38e892e11a6fd2184c
chptr_one <peter.vakulin@protonmail.com>
2021-05-08 15:47:53 +0300

    Add annotation and checks for null

--- src/main/java/chptr/one/ip_counter/IpStringHashFunction.java
+++ src/main/java/chptr/one/ip_counter/IpStringHashFunction.java
@@ -1,13 +1,17 @@
 package chptr.one.ip_counter;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Objects;
 import java.util.function.ToLongFunction;
 
 public class IpStringHashFunction implements ToLongFunction<String> {
 
     @Override
-    public long applyAsLong(String ipString) {
+    public long applyAsLong(@NotNull String ipString) {
+        Objects.requireNonNull(ipString);
         long result = 0;
         try {
             for (byte b : InetAddress.getByName(ipString).getAddress())
