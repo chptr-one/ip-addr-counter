9bf5723887297f5d7942a90fb2c947d5ac3265cb
chptr_one <peter.vakulin@protonmail.com>
2021-05-08 16:08:38 +0300

    More tests. Remove @NotNull

--- src/main/java/chptr/one/ip_counter/IpStringHashFunction.java
+++ src/main/java/chptr/one/ip_counter/IpStringHashFunction.java
@@ -1,7 +1,5 @@
 package chptr.one.ip_counter;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Objects;
@@ -10,7 +8,7 @@ import java.util.function.ToLongFunction;
 public class IpStringHashFunction implements ToLongFunction<String> {
 
     @Override
-    public long applyAsLong(@NotNull String ipString) {
+    public long applyAsLong(String ipString) {
         Objects.requireNonNull(ipString);
         long result = 0;
         try {
---
+++ src/test/java/chptr/one/ip_counter/IpStringHashFunctionTest.java
@@ -0,0 +1,32 @@
+package chptr.one.ip_counter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.function.ToLongFunction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class IpStringHashFunctionTest {
+
+    ToLongFunction<String> hashFunction = new IpStringHashFunction();
+    Map<String, Long> cornerCases = Map.of(
+            "0.0.0.0", 0L,
+            "255.255.255.255", (1L << 32) - 1
+    );
+
+    @Test
+    void doNotAllowNullArgument() {
+        assertThrows(NullPointerException.class, () -> hashFunction.applyAsLong(null));
+    }
+
+    @Test
+    void cornerCases() {
+        for (var testCase : cornerCases.entrySet()) {
+            long actual = hashFunction.applyAsLong(testCase.getKey());
+            long expected = testCase.getValue();
+            assertEquals(expected, actual);
+        }
+    }
+}
